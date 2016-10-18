6b52976f64456b5cdb91f2d34a6fe988e7e1ad2a
kswcarey <kswcarey@gmail.com>
2016-10-17 21:54:48 -0400

    Add item overlay and assign badges works
    Changes made for Assignment 4. These add functionality so that the add
    items process happens on a separate overlay, and the user can assign
    owners to tasks when the task is created. Few style changes this round.

--- myscript.js
+++ myscript.js
@@ -2,7 +2,6 @@
 var itemTemplate = 
     '<a href="#" class="list-group-item"> \
         <input type="checkbox" onclick="completeItem(this)" value=""> \
-        <span class="badge">A</span> \
         <button class="delete-button" onclick="deleteItem(this)">Delete</button></a>';
 
 //addItem function
@@ -12,9 +11,17 @@ var addItem = function () {
     var userInput = document.getElementById("text-input").value;
     //put text input into the innerHTML of the newItem
     newItem.append(userInput);
+    //check if any badges are selected
+    var selectedBadges = [$(".badge.badge-is-enabled").clone(true)];
+    //TODO create function that strips the badges in selectedBadges of their onclick listener
+    removeOnClick(selectedBadges);
+    //append newItem with the selected badge
+    newItem.append(selectedBadges);
     list.append(newItem);
-    //TODO clear the text input box
+    //clear the text input box
     document.getElementById("text-input").value = '';
+    //clear selected badges
+    resetBadges();
 };
 
 var deleteItem = function(element){
@@ -27,3 +34,41 @@ var completeItem = function(checked) {
     done.append(doneItem);
 }
 
+//open the add item overlay 
+var openOverlay = function(){
+     document.getElementById("add-item-overlay").style.width = "100%";
+}
+
+//close the add item overlay
+var closeOverlay = function (){
+    document.getElementById("add-item-overlay").style.width = "0%";
+}
+
+//Restyles clicked badge and prepare it to be added to a new item
+var selectBadge = function(element){
+    //when badge is clicked change styling of item
+    $(element).css("border-color", "red");
+    //append class of element to include "badge-is-enabled"
+    $(element).addClass("badge-is-enabled");
+}
+
+//removes onclick function from array of objects
+var removeOnClick = function(array){
+    var badges = $(array);
+    for (var i = 0, len = badges.length; i < len; i++){
+        badges[i].removeAttr("onclick");
+    }
+}
+
+//resets badges to default styling and class
+var resetBadges = function(){
+    $(".badge.badge-is-enabled").css("border-color", "grey");
+    //clear selected badges
+    $(".badge.badge-is-enabled").removeClass("badge-is-enabled");
+}
+
+//TODO create confirmation message when new item has been added
+var toaster = function(){
+    //prepend overlay div with confirmation message
+    //fade in and fade out
+}
